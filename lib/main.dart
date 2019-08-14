b6206dca0333f4ae1c28cb693a95da94498531fa
Waga <wagaodongo@gmail.com>
2019-08-14 09:00:58 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:insta_chat/screens/login_screen.dart';
+import 'package:insta_chat/screens/welcome_screen.dart';
+import 'package:insta_chat/screens/registration_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}

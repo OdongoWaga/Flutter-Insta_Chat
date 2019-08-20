108f0c65600855ae4767b0008e155b3eba56a7e9
Waga <wagaodongo@gmail.com>
2019-08-20 22:03:54 +0300

    Clean Up

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -37,20 +37,6 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-//  void getMessage() async {
-//    final messages = await _firestore.collection('messages').getDocuments();
-//    for (var message in messages.documents) {
-//      print(message.data);
-//    }
-//  }
-
-  void messagesStream() async {
-    await for (var snapshot in _firestore.collection('messages').snapshots())
-      for (var message in snapshot.documents) {
-        print(message.data);
-      }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,7 +46,6 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                messagesStream();
                 //_auth.signOut();
                 //Navigator.pop(context);
               }),
